a876a74fd250418f9a3ee524880124adcd7aa70d
DemyanchikVadim <vadim.demyanchik@gmail.com>
2017-02-16 22:40:04 +0300

    Adding validation url input using regex pattern

--- src/components/LinkForm.js
+++ src/components/LinkForm.js
@@ -29,7 +29,9 @@ class LinkForm extends React.Component {
     e.preventDefault();
     
     const errors = {};
+    const urlPattern = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/;
     if (this.state.link === '') errors.link = 'Input is empty';
+    if (!urlPattern.test(this.state.link) && this.state.link !== '') errors.link = 'It is not a valid url.';
     if (this.state.tags === '') errors.tags = 'Input is empty';
     this.setState({errors});
     const isValid = Object.keys(errors).length === 0;
@@ -48,41 +50,43 @@ class LinkForm extends React.Component {
   
   render() {
     return (
-      <div>
-        <form className={classnames('ui', 'form', {loading: this.state.loading})} onSubmit={this.handleSubmit}>
-          <h1>Add new link</h1>
-          
-          {this.state.errors.global && <div className="ui negative message"><p>{this.state.errors.global}</p></div> }
-          
-          <div className={classnames('field', {error: !!this.state.errors.link})}>
-            <label htmlFor='link'>Link</label>
-            <input
-              name='link'
-              value={this.state.link}
-              onChange={this.handleChange}
-              id='link'/>
-            <span>{this.state.errors.link}</span>
-          </div>
-          
-          <div className={classnames('field', {error: !!this.state.errors.tags})}>
-            <label htmlFor='tags'>Tags</label>
-            <input
-              name='tags'
-              value={this.state.tags}
-              onChange={this.handleChange}
-              id='tags'/>
-            <span>{this.state.errors.tags}</span>
-          </div>
-          <div className='field'>
-            <button className='ui primary button'>Enter</button>
-          </div>
-        </form>
-        <Link />
+      <div className="container">
+        <div className="shorten-url">
+          <form className={classnames('ui', 'form', {loading: this.state.loading})} onSubmit={this.handleSubmit}>
+            <h1>Add new link</h1>
+            
+            {this.state.errors.global && <div className="ui negative message"><p>{this.state.errors.global}</p></div> }
+            
+            <div className={classnames('field', {error: !!this.state.errors.link})}>
+              <label htmlFor='link'>Link</label>
+              <input
+                name='link'
+                value={this.state.link}
+                onChange={this.handleChange}
+                id='link'/>
+              <span>{this.state.errors.link}</span>
+            </div>
+            
+            <div className={classnames('field', {error: !!this.state.errors.tags})}>
+              <label htmlFor='tags'>Tags</label>
+              <input
+                name='tags'
+                value={this.state.tags}
+                onChange={this.handleChange}
+                id='tags'/>
+              <span>{this.state.errors.tags}</span>
+            </div>
+            <div className='field'>
+              <button className='ui primary button'>Enter</button>
+            </div>
+          </form>
+        </div>
+        <div className="shortened-link">
+          <Link />
+        </div>
       </div>
-    
-    
     );
   }
 }
 
-export default connect(null, { saveLink })(LinkForm);
+export default connect(null, {saveLink})(LinkForm);
