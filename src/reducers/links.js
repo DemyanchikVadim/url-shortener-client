b7df77f5de3ef4783f20a9b47ce833604479c73c
DemyanchikVadim <vadim.demyanchik@gmail.com>
2017-02-19 15:08:24 +0300

    Updating reducer for actions link fetched and link updated

--- src/reducers/links.js
+++ src/reducers/links.js
@@ -1,9 +1,30 @@
-import { SET_LINKS } from '../actions/AppActions';
+import { SET_LINKS, LINK_FETCHED, LINK_UPDATED } from '../actions/AppActions';
 
 export default function links(state = [], action = {}) {
   switch (action.type) {
+    case LINK_UPDATED:
+      return state.map(item => {
+        if (item._id === action.link._id) return action.link;
+        return item;
+      });
+    
+    case LINK_FETCHED:
+      const index = state.findIndex(item => item._id === action.link._id);
+      if (index > -1) {
+        return state.map(item => {
+          if (item._id === action.link._id) return action.link;
+          return item;
+        });
+      } else {
+        return [
+          ...state,
+          action.link
+        ]
+      }
+      
     case SET_LINKS:
       return action.links;
+    
     default: return state;
   }
 }
