88ead53cebd4dfcfd7d1b35a6f98966f3665c7e8
DemyanchikVadim <vadim.demyanchik@gmail.com>
2017-02-19 15:05:43 +0300

    Making put request to updating state link and add two actions

--- src/actions/AppActions.js
+++ src/actions/AppActions.js
@@ -1,15 +1,7 @@
 export const SET_LINKS = 'SET_LINKS';
 export const ADD_LINK = 'ADD_LINK';
-
-function handleResponse(response) {
-  if (response.ok) {
-    return response.json();
-  } else {
-    let error = new Error(response.statusText);
-    error.response = response;
-    throw error;
-  }
-}
+export const LINK_FETCHED = 'LINK_FETCHED';
+export const LINK_UPDATED = 'LINK_UPDATED';
 
 export function setLinks(links) {
   return {
@@ -25,9 +17,33 @@ export function addLink(link) {
   }
 }
 
+export function linkFetch(link) {
+  return {
+    type: LINK_FETCHED,
+    link
+  }
+}
+
+export function linkUpdated(link) {
+  return {
+    type: LINK_UPDATED,
+    link
+  }
+}
+
+function handleResponse(response) {
+  if (response.ok) {
+    return response.json();
+  } else {
+    let error = new Error(response.statusText);
+    error.response = response;
+    throw error;
+  }
+}
+
 export function saveLink(data) {
   return dispatch => {
-    return fetch('/links', {
+    return fetch('/api/link', {
       method: 'post',
       body: JSON.stringify(data),
       headers: {
@@ -38,10 +54,31 @@ export function saveLink(data) {
   }
 }
 
+export function updateLink(data) {
+  return dispatch => {
+    return fetch(`/api/links/${data._id}`, {
+      method: 'put',
+      body: JSON.stringify(data),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    }).then(handleResponse)
+    .then(data => dispatch(linkUpdated(data)));
+  }
+}
+
 export function fetchLinks() {
   return dispatch => {
-    fetch('/links')
+    fetch('/api/links')
     .then(res => res.json())
     .then(data => dispatch(setLinks(data)));
+  };
+}
+
+export function fetchLink(id) {
+  return dispatch => {
+    fetch(`/api/links/${id}`)
+    .then(res => res.json())
+    .then(data => dispatch(linkFetch(data)));
   };
 }
